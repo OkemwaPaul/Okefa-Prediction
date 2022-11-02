2a7b51cff729668464e9c4d84a15733ef604219a
OkemwaPaul <48824826+OkemwaPaul@users.noreply.github.com>
2022-11-02 16:10:00 +0300

    Add files via upload

---
+++ Welcome.jsx
@@ -0,0 +1,14 @@
+import { Link} from 'react-router-dom'
+
+function Welcome() {
+    return (
+        <div> 
+            <nav> 
+<h1>Welcome to Smart Mboka Welfare Association</h1>
+            </nav>
+        </div>
+
+    );
+
+}
+export default Welcome;
